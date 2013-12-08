b0cd6568928e0e0c549208b15fa02007217983fc
owidder <oliver.widder@gmail.com>
2013-12-08 08:35:39 +0100

    - ScrollBrickScene added

---
+++ yapg/brickScene/ScrollBrickScene.h
@@ -0,0 +1,13 @@
+//
+//  ScrollBrickScene.h
+//  yapg
+//
+//  Created by Oliver Widder on 26/11/13.
+//  Copyright (c) 2013 GeekAndPoke. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ScrollBrickScene : SKScene
+
+@end
---
+++ yapg/brickScene/ScrollBrickScene.m
@@ -0,0 +1,19 @@
+//
+//  ScrollBrickScene.m
+//  yapg
+//
+//  Created by Oliver Widder on 26/11/13.
+//  Copyright (c) 2013 GeekAndPoke. All rights reserved.
+//
+
+#import "ScrollBrickScene.h"
+
+@interface ScrollBrickScene() {
+    
+}
+
+@end
+
+@implementation ScrollBrickScene
+
+@end
